95c299df373b3cb6d7c96fc0e59a19ab22755227
sanshengshui <lovewsic@gmail.com>
2019-05-05 19:37:42 +0800

    update title description

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,8 +21,8 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
+  title: 'Groza', // Title for your website.
+  tagline: 'Groza is an open-source IoT Platform',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
